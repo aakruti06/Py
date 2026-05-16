68c46d051bb39d924e6aaa9f03708ad4dbe80936
aakruti06 <aakrutikmh@gmail.com>
2026-05-16 23:18:47 +0530

    practice projects

---
+++ eo.py
@@ -0,0 +1,10 @@
+def eo(n):
+	if n % 2 == 0:
+		print("even")
+	else:
+		print("odd")
+
+eo(5)
+print()
+eo(6)
+
---
+++ list_sum.py
@@ -0,0 +1,9 @@
+def s_list(lt):
+	total = 0
+	for num in lt:
+		total += num
+	return total
+
+print(s_list([5,4,3,2,1,0]))
+
+
